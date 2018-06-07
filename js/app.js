8be70a4524300f8e2ccb66e240ad369608885fcb
Brandon Haynes <brandonhaynes100@gmail.com>
2018-06-07 13:06:01 -0700

    syncing for pair programming

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // change variable to true to activate code for that section
-var overLoop = false;
-var questionOneThroughFiveActive = true;
+var overLoop = true;
+var questionOneThroughFiveActive = false;
 console.log('Questions 1-5 active:', questionOneThroughFiveActive);
-var questionSixActive = true;
+var questionSixActive = false;
 console.log('Question 6 active:', questionSixActive);
-var questionSevenActive = true;
+var questionSevenActive = false;
 console.log('Question 7 active:', questionSevenActive);
 
 // default answers for speedier testing
@@ -123,168 +123,45 @@ if(playGame === 'n' || playGame === null) {
         // retrieve and store the user's response to the current question
         var answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
         console.log('answerAttempt =', answerAttempt);
-
-        while(answerAttempt !== null && !(answerAttempt instanceof String)) {
-          // re-prompt, retrieve and store the user's response to the current question
-          var answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
-          console.log('answerAttempt =', answerAttempt);
-        }
-
+          
         // if the user selects cancel, break the loop and end the game
         if(answerAttempt === null) {
           console.log('User selected cancel');
           break;
         } 
 
-        // place the lowercase first letter of the user's answer into the currentUserAnswer variable
-        var currentUserAnswer = answerAttempt.toLowerCase.charAt(0);
-        // if the answer started with a y or an n, grade it
-        if(currentUserAnswer === 'n' || currentUserAnswer === 'y') {
-          // push the acceptable answer into the userAnswers array
-          userAnswers.push(currentUserAnswer);
-          // if their current answer is correct, tell them and store the accuracy
-          if(currentUserAnswer === currentMyAnswer) {
-            alert('Correct!');
-            userAccuracy.push(correct);
-          } else {
-            alert('Incorrect');
-            userAccuracy.push(incorrect);
-          }
-        
-        // if my answer is a number, proceed with q6 evaluation
-        } else if(myAnswers[i] instanceof Number) {
-          console.log('Number detected');
+        answerAttempt = answerAttempt.toLowerCase().charAt(0);
 
-        // if my answer is an array, proceed with q7 evaluation
-        } else if(myAnswers[i] instanceof Array) {
-          console.log('Array detected');
+        while(answerAttempt !== 'y' && answerAttempt !== 'n') {
+          // re-prompt, retrieve and store the user's response to the current question
+          var answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
+          console.log('answerAttempt =', answerAttempt);
+          // if the user selects cancel, break the loop and end the game
+          if(answerAttempt === null) {
+            console.log('User selected cancel');
+            break;
+          } 
+          answerAttempt = answerAttempt.toLowerCase().charAt(0);
         }
+
+        if(answerAttempt === currentMyAnswer) {
+          alert('Correct!');
+          userAccuracy.push(correct);
+        } else {
+          alert('Incorrect');
+          userAccuracy.push(incorrect);
+        }
+        
+      // if my answer is a number, proceed with q6 evaluation
+      } else if(i === 6) {
+        console.log('Number detected');
+
+      // if my answer is an array, proceed with q7 evaluation
+      } else if(i === 7) {
+        console.log('Array detected');
       }
     }// end for(var currentQuestion = 0; currentQuestion < myQuestions.length; currentQuestion++)
   }// end overLoop
-
-
-// CURRENT WORK AREA
-  // start questions 1-5
-  if(questionOneThroughFiveActive) {
-    for(i = 0; i < 5; i++) {
-      var currentUserAnswer = prompt(myQuestions[i]);
-      console.log('currentUserAnswer:', currentUserAnswer);
-      if(playGame === null) {
-        break;
-        console.log('User selected cancel.')
-      } else {
-        currentUserAnswer = currentUserAnswer.toLowerCase().charAt(0);
-        console.log('currentUserAnswer:', currentUserAnswer);
-      }
-
-      currentMyAnswer = myAnswers[i];
-      if(currentUserAnswer === currentMyAnswer) {
-        console.log("Correct!")
-        alert('Correct!')
-        userAccuracy.push(correct);
-      } else {
-        console.log("Incorrect.")
-        alert('Incorrect.')
-        userAccuracy.push(incorrect);
-      }
-    }
-  }// end questions 1-5
-  
-  // start question 6
-  if(questionSixActive) {
-    // Give the user 4 attempts for this question
-    userAttempts = 4;
-  
-    do {
-      // create a variable to store the user's response to the prompt containing question 6
-      var userAnsSix = parseInt(prompt(myQuestions[5] + ' Please enter an integer only. You have ' + userAttempts + ' attempts remaining.'));
-      console.log('userAnsSix =', userAnsSix);
-      // assign the correct answer to a local variable
-      var correctAnswerSix = myAnswers[5];
-
-      // indicate correct, too low, too high
-      if(userAnsSix === correctAnswerSix) {
-        // alert user
-        alert('That\'s correct!');
-        // load the correct answer into the userAnswers array
-        userAnswers.push(userAnsSix);
-        // update accuracy
-        userAccuracy.push(correct);
-        // leave the loop
-        break;
-      // if the user's guess was too low
-      } else if(userAnsSix < correctAnswerSix) {
-        // alert the user their guess was too low
-        alert('Oops, that\'s too low!');
-      // if the user's guess was too high
-      } else if(userAnsSix > correctAnswerSix) {
-        // alert the user their guess was too high
-        alert('Oops, that\'s too high!');
-      // the user's input was incorrect
-      } else {
-        // alert the user to enter a number only
-        alert('Oops, please only enter an integer!');
-      }
-      // if the user is out of attempts, this question is incorrect
-      if(userAttempts === 1) {
-        userAccuracy.push(incorrect);
-        console.log('pushed incorrect into userAccuracy');
-        alert('Sorry, you\'re out of attempts!');
-      }
-      userAttempts--;
-    } while(userAttempts > 0);
-  }// end question 6
-
-  // start question 7
-  if(questionSevenActive) {
-    // give the user 6 attempts for this question
-    userAttempts = 6
-    do {
-      // create a variable to store the user's response to the prompt containing question 6
-      var userAnsSeven = prompt(myQuestions[6] + ' You have ' + (userAttempts) + ' attempts remaining.').toLowerCase();
-      console.log('userAnsSeven =', userAnsSeven);
-      // create a variable to store the correct answer for question 7
-      var correctAnswerSeven = myAnswers[6];
-      console.log('correctAnswerSeven =', correctAnswerSeven);
-
-      // flag for whether the user's answer was found
-      var found = false;
-      // check whether the user's answer is in the answer array
-      for(var i = 0; i < correctAnswerSeven.length; i++) {
-        // if found, change flag and break out of the loop
-        if(correctAnswerSeven[i].toLowerCase() === userAnsSeven) {
-          found = true;
-          break;
-        }
-      }
-
-      // indicate correct or prompt to try again
-      if(found) {
-        // alert user
-        alert('That\'s correct!');
-        // load the correct answer into the userAnswers array
-        userAnswers.push(userAnsSeven);
-        // push correct into userAccuracy
-        userAccuracy.push(correct);
-        // leave the loop
-        break;
-      // if the user's guess wasn't present
-      } else {
-        // alert them their guess was incorrect
-        alert('Oops, that was incorrect.');
-        // push incorrect into userAccuracy
-        userAccuracy.push(incorrect);
-      }
-      // if the user is out of attempts, this question is incorrect
-      if(userAttempts === 1) {
-        userAccuracy.push(incorrect);
-        console.log('pushed incorrect into userAccuracy');
-        alert('Sorry, you\'re out of attempts!');
-      }
-      userAttempts--;
-    } while(userAttempts > 0);
-  }// end question 7
 
   // *END GAME*
 
@@ -299,7 +176,131 @@ if(playGame === 'n' || playGame === null) {
   }
   // tell user how well they did with a personalized, custom message, inlcuding their number of correct answers and their name
   alert('Congratulations, ' + userName + '! You got ' + userScore + ' questions correct!');
-  console.log('userScore:', userScore);
-} 
+  console.log('userScore:', userScore); 
+}// end else if(playGame == 'y')
+
+
+
+  // // start questions 1-5
+  // if(questionOneThroughFiveActive) {
+  //   for(i = 0; i < 5; i++) {
+  //     var currentUserAnswer = prompt(myQuestions[i]);
+  //     console.log('currentUserAnswer:', currentUserAnswer);
+  //     if(playGame === null) {
+  //       break;
+  //       console.log('User selected cancel.')
+  //     } else {
+  //       currentUserAnswer = currentUserAnswer.toLowerCase().charAt(0);
+  //       console.log('currentUserAnswer:', currentUserAnswer);
+  //     }
+
+  //     currentMyAnswer = myAnswers[i];
+  //     if(currentUserAnswer === currentMyAnswer) {
+  //       console.log("Correct!")
+  //       alert('Correct!')
+  //       userAccuracy.push(correct);
+  //     } else {
+  //       console.log("Incorrect.")
+  //       alert('Incorrect.')
+  //       userAccuracy.push(incorrect);
+  //     }
+  //   }
+  // }// end questions 1-5
+  
+  // // start question 6
+  // if(questionSixActive) {
+  //   // Give the user 4 attempts for this question
+  //   userAttempts = 4;
+  
+  //   do {
+  //     // create a variable to store the user's response to the prompt containing question 6
+  //     var userAnsSix = parseInt(prompt(myQuestions[5] + ' Please enter an integer only. You have ' + userAttempts + ' attempts remaining.'));
+  //     console.log('userAnsSix =', userAnsSix);
+  //     // assign the correct answer to a local variable
+  //     var correctAnswerSix = myAnswers[5];
+
+  //     // indicate correct, too low, too high
+  //     if(userAnsSix === correctAnswerSix) {
+  //       // alert user
+  //       alert('That\'s correct!');
+  //       // load the correct answer into the userAnswers array
+  //       userAnswers.push(userAnsSix);
+  //       // update accuracy
+  //       userAccuracy.push(correct);
+  //       // leave the loop
+  //       break;
+  //     // if the user's guess was too low
+  //     } else if(userAnsSix < correctAnswerSix) {
+  //       // alert the user their guess was too low
+  //       alert('Oops, that\'s too low!');
+  //     // if the user's guess was too high
+  //     } else if(userAnsSix > correctAnswerSix) {
+  //       // alert the user their guess was too high
+  //       alert('Oops, that\'s too high!');
+  //     // the user's input was incorrect
+  //     } else {
+  //       // alert the user to enter a number only
+  //       alert('Oops, please only enter an integer!');
+  //     }
+  //     // if the user is out of attempts, this question is incorrect
+  //     if(userAttempts === 1) {
+  //       userAccuracy.push(incorrect);
+  //       console.log('pushed incorrect into userAccuracy');
+  //       alert('Sorry, you\'re out of attempts!');
+  //     }
+  //     userAttempts--;
+  //   } while(userAttempts > 0);
+  // }// end question 6
+
+  // // start question 7
+  // if(questionSevenActive) {
+  //   // give the user 6 attempts for this question
+  //   userAttempts = 6
+  //   do {
+  //     // create a variable to store the user's response to the prompt containing question 6
+  //     var userAnsSeven = prompt(myQuestions[6] + ' You have ' + (userAttempts) + ' attempts remaining.').toLowerCase();
+  //     console.log('userAnsSeven =', userAnsSeven);
+  //     // create a variable to store the correct answer for question 7
+  //     var correctAnswerSeven = myAnswers[6];
+  //     console.log('correctAnswerSeven =', correctAnswerSeven);
+
+  //     // flag for whether the user's answer was found
+  //     var found = false;
+  //     // check whether the user's answer is in the answer array
+  //     for(var i = 0; i < correctAnswerSeven.length; i++) {
+  //       // if found, change flag and break out of the loop
+  //       if(correctAnswerSeven[i].toLowerCase() === userAnsSeven) {
+  //         found = true;
+  //         break;
+  //       }
+  //     }
+
+  //     // indicate correct or prompt to try again
+  //     if(found) {
+  //       // alert user
+  //       alert('That\'s correct!');
+  //       // load the correct answer into the userAnswers array
+  //       userAnswers.push(userAnsSeven);
+  //       // push correct into userAccuracy
+  //       userAccuracy.push(correct);
+  //       // leave the loop
+  //       break;
+  //     // if the user's guess wasn't present
+  //     } else {
+  //       // alert them their guess was incorrect
+  //       alert('Oops, that was incorrect.');
+  //       // push incorrect into userAccuracy
+  //       userAccuracy.push(incorrect);
+  //     }
+  //     // if the user is out of attempts, this question is incorrect
+  //     if(userAttempts === 1) {
+  //       userAccuracy.push(incorrect);
+  //       console.log('pushed incorrect into userAccuracy');
+  //       alert('Sorry, you\'re out of attempts!');
+  //     }
+  //     userAttempts--;
+  //   } while(userAttempts > 0);
+  // }// end question 7
+
 
 
