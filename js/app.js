89adf8b5fb20d62665189315ffc93529ce8174b8
Brandon Haynes <brandonhaynes100@gmail.com>
2018-06-07 18:14:59 -0700

    completing function wrapping of game code, restored to MVP

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 'use strict';
 
 // change variable to true to activate code for that section
-var overLoop = false;
-var questionOneThroughFiveActive = false;
-console.log('Questions 1-5 active:', questionOneThroughFiveActive);
-var questionSixActive = false;
-console.log('Question 6 active:', questionSixActive);
-var questionSevenActive = false;
-console.log('Question 7 active:', questionSevenActive);
+// var overLoop = false;
+
+var userName;
+var wantsToPlay;
+var userScore = 0;
 
 // default answers for speedier testing
 // change variable to true to turn on default answers
@@ -21,7 +19,6 @@ if(defaultAnswersOn) {
   var d6 = 5;
   var d7 = 'wombat?';
   var defaultAnswerArray = [d1, d2, d3, d4, d5, d6, d7];
-  var playGame;
 }
 console.log('defaultAnswersOn:', defaultAnswersOn);
 console.log(defaultAnswerArray);
@@ -53,7 +50,7 @@ var q5 = 'Do I bring lunch to Code Fellows?';
 var a5 = 'y';
 
 var q6 = 'I\'m thinking of a number between 0 and 10. Can you guess what it is?';
-var a6 = (Math.floor(Math.random() * 10));
+var a6 = (Math.floor(Math.random() * 10)) + 1;
 
 var q7 = 'Can you guess any of the 7 kung fu animal styles I\'ve studied in 6 or fewer attempts?';
 var a7 = ['Snake', 'Crane', 'Tiger', 'Mantis', 'Panther', 'Dragon', 'Monkey'];
@@ -61,114 +58,124 @@ var a7 = ['Snake', 'Crane', 'Tiger', 'Mantis', 'Panther', 'Dragon', 'Monkey'];
 var myQuestions = [q1, q2, q3, q4, q5, q6, q7];
 var myAnswers = [a1, a2, a3, a4, a5, a6, a7];
 
-// INTERACTIVITY START
+// FUNCTIONS
 
-// default name for speedier testing
-// change variable to true to turn on default name
-var userName = prompt('Hello! What is your name?', 'Guest');
+// start userNamePrompt()
+function userNamePrompt() {
+  // ask for the user's name, use a default if they hit cancel.
+  userName = prompt('Hello! Please enter your name or continue as a Guest', 'Guest');
+  // if the user selected cancel, leave userName null for playGamePrompt to catch
+  if(userName === null) {
+    console.log('User selected cancel at userNamePrompt');
+    return;
+  }
+  console.log('userName = ' + userName);
+}// end userNamePrompt()
 
-// if user selects cancel or enters nothing before continuing, assign the name of guest
-if(userName === null || userName === '') {
-  userName = 'Guest';
-}
-console.log('userName: ' + userName);
-
-function playGameYN() {
-  //record whether the user wants to play the game
-  var playGame = prompt('Hello, ' + userName + '. Do you want to play my guessing game? Please answer only with yes or no.');
-  console.log('playGame:', playGame);
-  if(playGame !== null) {
-    playGame = playGame.toLowerCase().charAt(0);
+// start playGamePrompt()
+function playGamePrompt() {
+  // if the user selected cancel, then exit the game process
+  if(userName === null) {
+    console.log('userName null at playGamePrompt');
+    return;
   }
 
+  do{
+    //record whether the user wants to play the game
+    wantsToPlay = prompt('Hello, ' + userName + '. Do you want to play my guessing game? Please answer only with yes or no.');
+    console.log('wantsToPlay:', wantsToPlay);
+    if(wantsToPlay !== null) {
+      // if not null, get the first letter in lowercase for evaluation
+      wantsToPlay = wantsToPlay.toLowerCase().charAt(0);
+    } else {
+      // if null, user selected cancel, exit prompt and don't start game
+      return;
+    }
+    if(wantsToPlay !== 'y' && wantsToPlay !== 'n') {
+      alert('Please answer only with yes or no.');
+    }
   // loop until the user submits a valid input, or hits cancel
-  while(playGame !== null && playGame !== 'y' && playGame !== 'n') {
-    alert('Please answer only with yes or no.');
-    playGame = prompt('Hello, ' + userName + '. Do you want to play my guessing game? Please answer only with yes or no').toLowerCase().charAt(0);
-    console.log('playGame:', playGame);
-    if(playGame === null) {
+  } while(wantsToPlay !== 'y' && wantsToPlay !== 'n');
+
+  if(wantsToPlay === 'y'){
+    // if user answered yes, start the game
+    gameStart();
+  } else {
+    // if they answered no, exit
+    return;
+  }
+}// end playGamePrompt()
+
+// start questions1thru5()
+function questions1thru5() {
+  for(var i = 0; i < 5; i++) {
+    var currentUserAnswer = prompt(myQuestions[i]);
+    console.log('currentUserAnswer:', currentUserAnswer);
+    if(currentUserAnswer === null) {
       console.log('User selected cancel.');
       break;
+    } else {
+      currentUserAnswer = currentUserAnswer.toLowerCase().charAt(0);
+      console.log('currentUserAnswer:', currentUserAnswer);
+    }
+    var currentMyAnswer = myAnswers[i];
+    if(currentUserAnswer === currentMyAnswer) {
+      console.log('Correct!');
+      alert('Correct!');
+      userAccuracy.push(correct);
+    } else {
+      console.log('Incorrect.');
+      alert('Incorrect.');
+      userAccuracy.push(incorrect);
     }
   }
-}
+}// end questions1thru5()
 
+// start question 6
+function question6() {
+  // Give the user 4 attempts for this question
+  userAttempts = 4;
 
+  do {
+    // create a variable to store the user's response to the prompt containing question 6
+    var userAnsSix = parseInt(prompt(myQuestions[5] + ' Please enter an integer only. You have ' + userAttempts + ' attempts remaining.'));
+    console.log('userAnsSix =', userAnsSix);
+    // assign the correct answer to a local variable
+    var correctAnswerSix = myAnswers[5];
 
-// if user replies no or hits cancel, send a simple message and do not start the game
-if(playGame === 'n' || playGame === null) {
-  console.log('playGame:', playGame);
-  alert('Have a great day, ' + userName + '!');
-// if user replies yes, begin the game
-} else if(playGame === 'y') {
-  console.log('Game Start!');
+    // indicate correct, too low, too high
+    if(userAnsSix === correctAnswerSix) {
+      // alert user
+      alert('That\'s correct!');
+      // load the correct answer into the userAnswers array
+      userAnswers.push(userAnsSix);
+      // update accuracy
+      userAccuracy.push(correct);
+      // leave the loop
+      break;
+    // if the user's guess was too low
+    } else if(userAnsSix < correctAnswerSix) {
+      // alert the user their guess was too low
+      alert('Oops, that\'s too low!');
+    // if the user's guess was too high
+    } else if(userAnsSix > correctAnswerSix) {
+      // alert the user their guess was too high
+      alert('Oops, that\'s too high!');
+    // the user's input was incorrect
+    } else {
+      // alert the user to enter a number only
+      alert('Oops, please only enter an integer!');
+    }
+    // if the user is out of attempts, this question is incorrect
+    if(userAttempts === 1) {
+      userAccuracy.push(incorrect);
+      console.log('pushed incorrect into userAccuracy');
+      alert('Sorry, you\'re out of attempts!');
+    }
+    userAttempts--;
+  } while(userAttempts > 0);
+}// end question 6
 
-  // *START GAME*
-
-  // loop to go through all the questions
-
-  if(overLoop) {
-    for(var i = 0; i < myQuestions.length; i++) {
-      var currentMyQuestion = myQuestions[i];
-      console.log(', currentMyQuestion');
-      var currentMyAnswer = myAnswers[i];
-      console.log('currentMyAnswer =', currentMyAnswer);
-      var defaultAnswer = '';
-      if(defaultAnswersOn) {
-        defaultAnswer = defaultAnswerArray[i];
-      }
-
-      // if the current question index is less than 6, we're answering yes/no questions, proceed with y and n evaluation
-      if(i < 6) {
-        console.log('q1-q5 detected');
-
-        // retrieve and store the user's response to the current question
-        var answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
-        console.log('answerAttempt =', answerAttempt);
-
-        // if the user selects cancel, break the loop and end the game
-        if(answerAttempt === null) {
-          console.log('User selected cancel');
-          break;
-        }
-
-        answerAttempt = answerAttempt.toLowerCase().charAt(0);
-
-        while(answerAttempt !== 'y' && answerAttempt !== 'n') {
-          // re-prompt, retrieve and store the user's response to the current question
-          answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
-          console.log('answerAttempt =', answerAttempt);
-          // if the user selects cancel, break the loop and end the game
-          if(answerAttempt === null) {
-            console.log('User selected cancel');
-            break;
-          }
-          answerAttempt = answerAttempt.toLowerCase().charAt(0);
-        }
-
-        if(answerAttempt === currentMyAnswer) {
-          alert('Correct!');
-          userAccuracy.push(correct);
-        } else {
-          alert('Incorrect');
-          userAccuracy.push(incorrect);
-        }
-
-      // if my answer is a number, proceed with q6 evaluation
-      } else if(i === 6) {
-        console.log('Number detected');
-
-      // if my answer is an array, proceed with q7 evaluation
-      } else if(i === 7) {
-        console.log('Array detected');
-      }
-    }// end for(var currentQuestion = 0; currentQuestion < myQuestions.length; currentQuestion++)
-  }// end overLoop
-
-  // *END GAME*
-
-
-}
 // start question 7
 function question7() {
   // give the user 6 attempts for this question
@@ -222,10 +229,8 @@ function question7() {
 function endGameSummary() {
 
   // game-end events
-  // variable for user's number of correct answers
-  var userScore = 0;
   // calculate the number of correct answers
-  for(i = 0; i < userAccuracy.length; i++) {
+  for(var i = 0; i < userAccuracy.length; i++) {
     if(userAccuracy[i] === correct) {
       userScore++;
     }
@@ -236,80 +241,74 @@ function endGameSummary() {
 
 
 }
-question7();
-endGameSummary();
-playGameYN();
+
+userNamePrompt();
+playGamePrompt();
+function gameStart() {
+  questions1thru5();
+  question6();
+  question7();
+  endGameSummary();
+}
 
 
-// // start questions 1-5
-// if(questionOneThroughFiveActive) {
-//   for(i = 0; i < 5; i++) {
-//     var currentUserAnswer = prompt(myQuestions[i]);
-//     console.log('currentUserAnswer:', currentUserAnswer);
-//     if(playGame === null) {
-//       break;
-//       console.log('User selected cancel.')
-//     } else {
-//       currentUserAnswer = currentUserAnswer.toLowerCase().charAt(0);
-//       console.log('currentUserAnswer:', currentUserAnswer);
-//     }
 
-//     currentMyAnswer = myAnswers[i];
-//     if(currentUserAnswer === currentMyAnswer) {
-//       console.log("Correct!")
-//       alert('Correct!')
-//       userAccuracy.push(correct);
-//     } else {
-//       console.log("Incorrect.")
-//       alert('Incorrect.')
-//       userAccuracy.push(incorrect);
-//     }
-//   }
-// }// end questions 1-5
 
-// // start question 6
-// if(questionSixActive) {
-//   // Give the user 4 attempts for this question
-//   userAttempts = 4;
+// loop to go through all the questions
+//   if(overLoop) {
+//     for(var i = 0; i < myQuestions.length; i++) {
+//       var currentMyQuestion = myQuestions[i];
+//       console.log(', currentMyQuestion');
+//       var currentMyAnswer = myAnswers[i];
+//       console.log('currentMyAnswer =', currentMyAnswer);
+//       var defaultAnswer = '';
+//       if(defaultAnswersOn) {
+//         defaultAnswer = defaultAnswerArray[i];
+//       }
 
-//   do {
-//     // create a variable to store the user's response to the prompt containing question 6
-//     var userAnsSix = parseInt(prompt(myQuestions[5] + ' Please enter an integer only. You have ' + userAttempts + ' attempts remaining.'));
-//     console.log('userAnsSix =', userAnsSix);
-//     // assign the correct answer to a local variable
-//     var correctAnswerSix = myAnswers[5];
+//       // if the current question index is less than 6, we're answering yes/no questions, proceed with y and n evaluation
+//       if(i < 6) {
+//         console.log('q1-q5 detected');
 
-//     // indicate correct, too low, too high
-//     if(userAnsSix === correctAnswerSix) {
-//       // alert user
-//       alert('That\'s correct!');
-//       // load the correct answer into the userAnswers array
-//       userAnswers.push(userAnsSix);
-//       // update accuracy
-//       userAccuracy.push(correct);
-//       // leave the loop
-//       break;
-//     // if the user's guess was too low
-//     } else if(userAnsSix < correctAnswerSix) {
-//       // alert the user their guess was too low
-//       alert('Oops, that\'s too low!');
-//     // if the user's guess was too high
-//     } else if(userAnsSix > correctAnswerSix) {
-//       // alert the user their guess was too high
-//       alert('Oops, that\'s too high!');
-//     // the user's input was incorrect
-//     } else {
-//       // alert the user to enter a number only
-//       alert('Oops, please only enter an integer!');
-//     }
-//     // if the user is out of attempts, this question is incorrect
-//     if(userAttempts === 1) {
-//       userAccuracy.push(incorrect);
-//       console.log('pushed incorrect into userAccuracy');
-//       alert('Sorry, you\'re out of attempts!');
-//     }
-//     userAttempts--;
-//   } while(userAttempts > 0);
-// }// end question 6
+//         // retrieve and store the user's response to the current question
+//         var answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
+//         console.log('answerAttempt =', answerAttempt);
 
-// end else if(playGame == 'y')
+//         // if the user selects cancel, break the loop and end the game
+//         if(answerAttempt === null) {
+//           console.log('User selected cancel');
+//           break;
+//         }
+
+//         answerAttempt = answerAttempt.toLowerCase().charAt(0);
+
+//         while(answerAttempt !== 'y' && answerAttempt !== 'n') {
+//           // re-prompt, retrieve and store the user's response to the current question
+//           answerAttempt = prompt(currentMyQuestion + ' Please only answer with yes or no.', defaultAnswer);
+//           console.log('answerAttempt =', answerAttempt);
+//           // if the user selects cancel, break the loop and end the game
+//           if(answerAttempt === null) {
+//             console.log('User selected cancel');
+//             break;
+//           }
+//           answerAttempt = answerAttempt.toLowerCase().charAt(0);
+//         }
+
+//         if(answerAttempt === currentMyAnswer) {
+//           alert('Correct!');
+//           userAccuracy.push(correct);
+//         } else {
+//           alert('Incorrect');
+//           userAccuracy.push(incorrect);
+//         }
+
+//       // if my answer is a number, proceed with q6 evaluation
+//       } else if(i === 6) {
+//         console.log('Number detected');
+
+//       // if my answer is an array, proceed with q7 evaluation
+//       } else if(i === 7) {
+//         console.log('Array detected');
+//       }
+//     }// end for(var currentQuestion = 0; currentQuestion < myQuestions.length; currentQuestion++)
+//   }// end overLoop
